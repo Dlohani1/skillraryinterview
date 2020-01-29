5088b824a65aba4fdec7a7e312d901135cd1ec97
deepak.lohani <deepak.lohani@skillrary.com>
2020-01-29 17:14:00 +0530

    keyboard & mouse click restricted

--- application/views/mcq_form.php
+++ application/views/mcq_form.php
@@ -596,6 +596,53 @@ function clearResponse() {
 
 </script>
  <script>
+
+// function disableF5(e) { if ((e.which || e.keyCode) == 116 || 82) e.preventDefault(); };
+// $(document).on("keydown", disableF5);
+
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "I" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        disabledEvent(e);
+      }
+       // "R" key
+       if (e.ctrlKey && e.shiftKey && e.keyCode == 82) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey && e.keyCode == 85) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
+
+
     var countdownTimer;
 
     function setTime() {
