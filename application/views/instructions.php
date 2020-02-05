fcc8be062ccc89c77aa16c25718ac40f55199b52
deepak.lohani <deepak.lohani@skillrary.com>
2020-02-05 15:43:43 +0530

    window popup width and height has added

--- application/views/instructions.php
+++ application/views/instructions.php
@@ -238,9 +238,9 @@
                 var remember = document.getElementById("checkbox-1");
                 if (remember.checked) {
                 //window.location.href='mcq-question';
-                // var win = window.open("mcq-question", "","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000");
+                var win = window.open("mcq-question", "","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000");
 
-                    var win = window.open("mcq-question","", "fullscreen=1,directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no");
+                    // var win = window.open("mcq-question","", "fullscreen=1,directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no");
                     win.onbeforeunload = function(){
                         console.log('unload');
                         window.location.href="user/view-results";
