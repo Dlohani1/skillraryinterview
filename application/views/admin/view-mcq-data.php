c192a4086a404374fe9b1006bf130a45a4ad9c1a
deepak <deepak.lohani@skillrary.com>
2020-04-24 09:34:24 +0530

    fixed

--- application/views/admin/view-mcq-data.php
+++ application/views/admin/view-mcq-data.php
@@ -3,6 +3,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 <style>
   /* Style the container/contact section */
 #detail {
